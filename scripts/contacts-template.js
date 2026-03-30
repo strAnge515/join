0622061df6ed2514b56a3247e2967d3f093d79d8
Kathy <kathleenhunt94@gmail.com>
2026-03-30 23:22:33 +0200

    edit img src change header and sidebar styles merged contact content to header/sidebar

--- scripts/contacts-template.js
+++ scripts/contacts-template.js
@@ -21,10 +21,10 @@ function getContactDetailTemplate(contact, initials, color) {
           <div class="detail-name">${contact.firstName} ${contact.lastName}</div>
           <div class="detail-actions">
           <button class="edit-btn" onclick="openEditContactDialog(${contact.id})">
-          <img src="assets/img/contacts/edit.svg" alt="">
+          <img src="../assets/img/contacts/edit.svg" alt="">
           Edit </button>
           <button class="edit-btn" onclick="deleteContact(${contact.id})">
-           <img src="assets/img/contacts/delete.svg" alt="">
+           <img src="../assets/img/contacts/delete.svg" alt="">
            Delete</button>
            </div>
         </div>
@@ -47,7 +47,7 @@ function getContactDetailTemplate(contact, initials, color) {
 function getEditContactTemplate(contact, initials, color) {
   return `<div class="dialog">
         <div class="dialog-left">
-          <img src="./assets/img/contacts/join-logo.svg" class="logo" />
+          <img src="../assets/img/contacts/join-logo.svg" class="logo" />
           <h1>Edit contact</h1>
           <div class="underline"></div>
         </div>
@@ -59,15 +59,15 @@ function getEditContactTemplate(contact, initials, color) {
             <button class="close-btn" onclick="closeDialog(this)">✕</button>
             <div class="input-wrapper">
               <input id="nameInput" type="text" placeholder="Name" value="${contact.firstName} ${contact.lastName}" required />
-              <img src="assets/img/contacts/person.svg" alt="">
+              <img src="../assets/img/contacts/person.svg" alt="">
             </div>
             <div class="input-wrapper">
               <input id="emailInput" type="email" placeholder="Email" value="${contact.email}" required />
-              <img src="assets/img/contacts/mail.svg" alt="">
+              <img src="../assets/img/contacts/mail.svg" alt="">
             </div>
             <div class="input-wrapper">
               <input id="phoneInput" type="tel" placeholder="Phone" value="${contact.phone}" required />
-              <img src="assets/img/contacts/call.svg" alt="">
+              <img src="../assets/img/contacts/call.svg" alt="">
             </div>
             <div class="action-btns">
               <button class="cancel-btn" onclick="closeDialog(this)">
